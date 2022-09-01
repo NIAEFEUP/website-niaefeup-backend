73fdfa3120919586bad5d44e4350dd6eff494fca
André Ávila <avila_andre14@hotmail.com>
2022-09-01 22:30:18 +0100

    Get Single Post and not found exception handler.

--- src/main/kotlin/pt/up/fe/ni/website/backend/controller/PostController.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/controller/PostController.kt
@@ -1,18 +1,25 @@
 package pt.up.fe.ni.website.backend.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import pt.up.fe.ni.website.backend.model.Post
 import pt.up.fe.ni.website.backend.service.PostService
+import kotlin.NoSuchElementException
 
 @RestController
 @RequestMapping("/posts")
 class PostController(private val service: PostService) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e : NoSuchElementException): ResponseEntity<String> =
+            ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
     @GetMapping
     fun getAllPosts(): Collection<Post> = service.getAllPosts()
+
+    @GetMapping("/{postID}")
+    fun getPost(@PathVariable postID: String): Post = service.getPost(postID)
 
     @PostMapping("/new")
     fun createPost(@RequestBody post: Post) = service.createPost(post)
--- src/main/kotlin/pt/up/fe/ni/website/backend/service/PostService.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/service/PostService.kt
@@ -1,5 +1,6 @@
 package pt.up.fe.ni.website.backend.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import pt.up.fe.ni.website.backend.model.Post
 import pt.up.fe.ni.website.backend.repository.PostRepository
@@ -7,6 +8,8 @@ import pt.up.fe.ni.website.backend.repository.PostRepository
 @Service
 class PostService(private val repository: PostRepository) {
     fun getAllPosts(): Collection<Post> = repository.findAll().toList()
+
+    fun getPost(postID : String): Post = repository.findByIdOrNull(postID.toLong()) ?: throw NoSuchElementException("Post Not Found")
 
     fun createPost(post: Post) = repository.save(post)
 }
