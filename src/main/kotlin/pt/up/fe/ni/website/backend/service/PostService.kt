57e38932e88d0e14489b12b70c71066a202a636a
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-09-18 22:29:44 +0100

    Added id to post not found error

--- src/main/kotlin/pt/up/fe/ni/website/backend/service/PostService.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/service/PostService.kt
@@ -10,12 +10,13 @@ class PostService(private val repository: PostRepository) {
     fun getAllPosts(): List<Post> = repository.findAll().toList()
 
     fun getPost(PostId: Long): Post =
-        repository.findByIdOrNull(PostId) ?: throw NoSuchElementException("Post Not Found")
+        repository.findByIdOrNull(PostId) ?: throw NoSuchElementException("post not found with id $PostId")
 
     fun createPost(post: Post): Post = repository.save(post)
 
     fun updatePost(PostId: Long, post: Post.PatchModel): Post {
-        val targetPost = repository.findByIdOrNull(PostId) ?: throw NoSuchElementException("Post Not Found")
+        val targetPost =
+            repository.findByIdOrNull(PostId) ?: throw NoSuchElementException("post not found with id $PostId")
         targetPost.title = post.title ?: targetPost.title
         targetPost.body = post.body ?: targetPost.body
         targetPost.thumbnailPath = post.thumbnailPath ?: targetPost.thumbnailPath
@@ -24,7 +25,7 @@ class PostService(private val repository: PostRepository) {
     }
 
     fun deletePost(PostId: Long): Map<String, String> {
-        repository.findByIdOrNull(PostId) ?: throw NoSuchElementException("Post Not Found")
+        repository.findByIdOrNull(PostId) ?: throw NoSuchElementException("post not found with id $PostId")
         repository.deleteById(PostId)
 
         return mapOf()
