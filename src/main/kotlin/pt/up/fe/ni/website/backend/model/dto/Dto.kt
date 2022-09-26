f6065a196a574724dcb1fd1613ef77ab20767a21
André Lima <me@limwa.pt>
2022-09-26 17:49:08 +0100

    refactor: add comments and remove useless annotations

--- src/main/kotlin/pt/up/fe/ni/website/backend/model/dto/Dto.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/model/dto/Dto.kt
@@ -49,12 +49,53 @@ open class Dto<T : Any> {
     object DtoReflectionUtils {
         private val typeArgumentCache = HashMap<KClass<out Dto<*>>, KClass<*>>()
 
+        /*
+         * @Suppress("UNCHECKED_CAST") is a hint to the compiler to suppress all warnings which are related to unchecked
+         * casts.
+         *
+         * When you cast an object to a string, the JVM checks if that object really is a String before proceeding.
+         * If it's not, it'll throw a cast exception. That's the reason why you should always use instanceof before any cast.
+         *
+         * But what about List? What happens if I want to convert a List<Integer> to a List<String>? You can't really
+         * cast a String to an Integer, but generics don't really exist at runtime, so, in practice, you'd be converting
+         * from List to List, which seems valid to the JVM. It doesn't throw a runtime error. That's an unchecked cast.
+         *
+         * Here, we are converting from KClass<*> to KClass<T>, which follows the same logic.
+         *
+         * ```
+         * val strings: List<String> = listOf("one", "two", "three")
+         * println(strings.joinToString(", "))
+         *
+         * val numbers: List<Int> = strings as List<Int>
+         * println(numbers.joinToString(", "))
+         * ```
+         *
+         * This code runs without errors and the output is
+         * ```
+         * one, two, three
+         * one, two, three
+         * ```
+         */
         @Suppress("UNCHECKED_CAST")
         private fun <T : Any> getTypeConversionClass(clazz: KClass<out Dto<T>>): KClass<T>? {
             val thisType = clazz.supertypes.first { it.classifier == Dto::class }
+
+            /*
+             * I don't really know *how* jvmErasure works...
+             * It's a Kotlin Reflect feature that seems to completely recover generic type information at runtime.
+             * We can use it to determine the class of T, the type argument of Dto.
+             */
             return thisType.arguments.firstOrNull()?.type?.jvmErasure as KClass<T>?
         }
 
+        /*
+         * See ValidationUtils#getTypeConversionClass for information about @Suppress("UNCHECKED_CAST")
+         *
+         *  There is a performance hit when using reflection. Reflective operations take a lot of time since they can't
+         * be optimized by the Java compiler at compile-time. As a rule of thumb, you should minimize the amount of
+         * reflective operations in your code, and since the type argument stays the same for every different kind of
+         * Dto, we can cache it, thus reducing the amount of reflective operations performed.
+         */
         @Suppress("UNCHECKED_CAST")
         fun <T : Any> getTypeConversionClassWithCache(clazz: KClass<out Dto<T>>, fallback: KClass<T>? = null): KClass<T> {
             if (clazz == Dto::class) throw IllegalCallerException("DTO is not extended by any class")
