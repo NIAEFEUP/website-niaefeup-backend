b9398c91c3dd8530c2aba5ef8f45059a90e8f00a
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-10-02 18:23:06 +0100

    tests for updating posts

--- src/test/kotlin/pt/up/fe/ni/website/backend/controller/PostControllerTest.kt
+++ src/test/kotlin/pt/up/fe/ni/website/backend/controller/PostControllerTest.kt
@@ -1,8 +1,9 @@
 package pt.up.fe.ni.website.backend.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.hamcrest.Matchers.emptyString
 import org.hamcrest.Matchers.not
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.put
 import pt.up.fe.ni.website.backend.model.Post
 import pt.up.fe.ni.website.backend.repository.PostRepository
 import java.util.Date
@@ -112,8 +114,8 @@ internal class PostControllerTest @Autowired constructor(
                     jsonPath("$.title") { value(testPost.title) }
                     jsonPath("$.body") { value(testPost.body) }
                     jsonPath("$.thumbnailPath") { value(testPost.thumbnailPath) }
-                    jsonPath("$.publishDate") { value(not(emptyString())) }
-                    jsonPath("$.lastUpdatedAt") { value(not(emptyString())) }
+                    jsonPath("$.publishDate") { exists() }
+                    jsonPath("$.lastUpdatedAt") { exists() }
                 }
         }
 
@@ -212,6 +214,137 @@ internal class PostControllerTest @Autowired constructor(
                 content { contentType(MediaType.APPLICATION_JSON) }
                 jsonPath("$.errors.length()") { value(1) }
                 jsonPath("$.errors[0].message") { value("post not found with id 1234") }
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("PUT /posts/{postId}")
+    inner class UpdatePost {
+        @BeforeEach
+        fun addEvent() {
+            repository.save(testPost)
+        }
+
+        @Test
+        fun `should update the post`() {
+            val newTitle = "New Title"
+            val newBody = "New Body of the post"
+            val newThumbnailPath = "thumbnails/new.png"
+
+            mockMvc.put("/posts/${testPost.id}") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(
+                    mapOf(
+                        "title" to newTitle,
+                        "body" to newBody,
+                        "thumbnailPath" to newThumbnailPath
+                    )
+                )
+            }
+                .andExpect {
+                    status { isOk() }
+                    content { contentType(MediaType.APPLICATION_JSON) }
+                    jsonPath("$.title") { value(newTitle) }
+                    jsonPath("$.body") { value(newBody) }
+                    jsonPath("$.thumbnailPath") { value(newThumbnailPath) }
+                    jsonPath("$.publishDate") { value(testPost.publishDate.toJson()) }
+                    jsonPath("$.lastUpdatedAt") { value(not(testPost.lastUpdatedAt.toJson())) }
+                }
+
+            val updatedPost = repository.findById(testPost.id!!).get()
+            assertEquals(newTitle, updatedPost.title)
+            assertEquals(newBody, updatedPost.body)
+            assertEquals(newThumbnailPath, updatedPost.thumbnailPath)
+            assertEquals(testPost.publishDate, updatedPost.publishDate)
+            assertNotEquals(testPost.lastUpdatedAt, updatedPost.lastUpdatedAt)
+        }
+
+        @Test
+        fun `should fail if the post does not exist`() {
+            mockMvc.put("/posts/1234") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(
+                    mapOf(
+                        "title" to "New Title",
+                        "body" to "New Body of the post",
+                        "thumbnailPath" to "thumbnails/new.png"
+                    )
+                )
+            }
+                .andExpect {
+                    status { isNotFound() }
+                    content { contentType(MediaType.APPLICATION_JSON) }
+                    jsonPath("$.errors.length()") { value(1) }
+                    jsonPath("$.errors[0].message") { value("post not found with id 1234") }
+                }
+        }
+
+        @Nested
+        @DisplayName("Input Validation")
+        inner class InputValidation {
+            private val validationTester = ValidationTester(
+                req = { params: Map<String, Any> ->
+                    mockMvc.put("/posts/${testPost.id}") {
+                        contentType = MediaType.APPLICATION_JSON
+                        content = objectMapper.writeValueAsString(params)
+                    }
+                },
+                requiredFields = mapOf(
+                    "title" to testPost.title,
+                    "body" to testPost.body,
+                    "thumbnailPath" to testPost.thumbnailPath
+                )
+            )
+
+            @Nested
+            @DisplayName("title")
+            @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+            inner class TitleValidation {
+                @BeforeAll
+                fun setParam() {
+                    validationTester.param = "title"
+                }
+
+                @Test
+                fun `should be required`() = validationTester.isRequired()
+
+                @Test
+                @DisplayName("size should be between ${Constants.Title.minSize} and ${Constants.Title.maxSize}()")
+                fun size() = validationTester.hasSizeBetween(Constants.Title.minSize, Constants.Title.maxSize)
+            }
+
+            @Nested
+            @DisplayName("body")
+            @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+            inner class BodyValidation {
+                @BeforeAll
+                fun setParam() {
+                    validationTester.param = "body"
+                }
+
+                @Test
+                fun `should be required`() = validationTester.isRequired()
+
+                @Test
+                @DisplayName("size must be greater or equal to ${Constants.Body.minSize}()")
+                fun size() = validationTester.hasMinSize(Constants.Body.minSize)
+            }
+
+            @Nested
+            @DisplayName("thumbnailPath")
+            @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+            inner class ThumbnailValidation {
+                @BeforeAll
+                fun setParam() {
+                    validationTester.param = "thumbnailPath"
+                }
+
+                @Test
+                fun `should be required`() = validationTester.isRequired()
+
+                @Test
+                fun `should not be empty`() = validationTester.isNotEmpty()
             }
         }
     }
