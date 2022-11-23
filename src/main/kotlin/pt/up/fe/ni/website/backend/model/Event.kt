ddf711d0ff96249aaa82fe707c8a120e0ea0188a
toino <joaoapereira21@hotmail.com>
2022-11-23 16:32:43 +0000

    Add more properties and endpoints to event

--- src/main/kotlin/pt/up/fe/ni/website/backend/model/Event.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/model/Event.kt
@@ -2,9 +2,12 @@ package pt.up.fe.ni.website.backend.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Entity
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
 import org.hibernate.validator.constraints.URL
 import pt.up.fe.ni.website.backend.annotations.validation.NullOrNotBlank
 import java.util.Date
+import pt.up.fe.ni.website.backend.model.constants.EventConstants as Constants
 
 @Entity
 class Event(
@@ -16,7 +19,24 @@ class Event(
     var registerUrl: String? = null,
 
     @JsonProperty(required = true)
-    val date: Date,
+    val startDate: Date,
+
+    val endDate: Date?,
+
+    @field:NullOrNotBlank
+    @field:URL
+    val url: String?,
+
+    @field:Size(min = Constants.Location.minSize, max = Constants.Location.maxSize)
+    val location: String?,
+
+    @field:Size(min = Constants.Category.minSize, max = Constants.Category.maxSize)
+    val category: String?,
+
+    @JsonProperty(required = true)
+    @field:NotEmpty
+    @field:URL
+    val thumbnailPath: String,
 
     id: Long? = null
 ) : Activity(title, description, id)
