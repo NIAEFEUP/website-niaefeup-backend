60d94aa9a2cf56ac3339fff28c54a548cd985a2f
toino <joaoapereira21@hotmail.com>
2022-12-07 15:03:07 +0000

    Implement DateInterval annotation
    Checks if a date comes after another in the same class

--- src/test/kotlin/pt/up/fe/ni/website/backend/controller/EventControllerTest.kt
+++ src/test/kotlin/pt/up/fe/ni/website/backend/controller/EventControllerTest.kt
@@ -289,6 +289,9 @@ internal class EventControllerTest @Autowired constructor(
 
                 @Test
                 fun `should be a Date`() = validationTester.isDate()
+
+                @Test
+                fun `should be after startDate`() = validationTester.isAfter("startDate")
             }
 
             @NestedTest
