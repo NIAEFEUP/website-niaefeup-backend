ff3ddd5239f6e2c06f56c585c0328058ed033bf1
André Lima <me@limwa.pt>
2023-01-09 04:04:45 +0000

    feat: add service to manage role permissions

---
+++ src/main/kotlin/pt/up/fe/ni/website/backend/service/RoleService.kt
@@ -0,0 +1,58 @@
+package pt.up.fe.ni.website.backend.service
+
+import org.springframework.stereotype.Service
+import pt.up.fe.ni.website.backend.model.Activity
+import pt.up.fe.ni.website.backend.model.PerActivityRole
+import pt.up.fe.ni.website.backend.model.Role
+import pt.up.fe.ni.website.backend.permissions.Permission
+import pt.up.fe.ni.website.backend.permissions.Permissions
+import pt.up.fe.ni.website.backend.repository.PerActivityRoleRepository
+import pt.up.fe.ni.website.backend.repository.RoleRepository
+
+@Service
+class RoleService(private val roleRepository: RoleRepository, private val perActivityRoleRepository: PerActivityRoleRepository) {
+
+    fun grantPermissionToRole(role: Role, permission: Permission) {
+        role.permissions.add(permission)
+        roleRepository.save(role)
+    }
+
+    fun revokePermissionFromRole(role: Role, permission: Permission) {
+        role.permissions.remove(permission)
+        roleRepository.save(role)
+    }
+
+    fun grantPermissionToRoleOnActivity(role: Role, activity: Activity, permission: Permission) {
+        var foundActivity: PerActivityRole? = null
+
+        for (perActivity in activity.perRoles) {
+            if (perActivity.activity == activity) {
+                foundActivity = perActivity
+                break
+            }
+        }
+
+        if (foundActivity == null) {
+            foundActivity = PerActivityRole(role, activity, Permissions())
+        }
+
+        foundActivity.permissions.add(permission)
+        perActivityRoleRepository.save(foundActivity)
+    }
+
+    fun revokePermissionToRoleOnActivity(role: Role, activity: Activity, permission: Permission) {
+        var foundActivity: PerActivityRole? = null
+
+        for (perActivity in activity.perRoles) {
+            if (perActivity.activity == activity) {
+                foundActivity = perActivity
+                break
+            }
+        }
+
+        if (foundActivity != null) {
+            foundActivity.permissions.remove(permission)
+            perActivityRoleRepository.save(foundActivity)
+        }
+    }
+}
