29b6fbf163954e78a31bb62e5fd1bfc35ff2af87
André Lima <me@limwa.pt>
2023-01-09 02:45:10 +0000

    style: fix indentation

--- src/main/kotlin/pt/up/fe/ni/website/backend/model/Activity.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/model/Activity.kt
@@ -6,26 +6,26 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Inheritance
 import jakarta.persistence.InheritanceType
+import jakarta.persistence.OneToMany
 import jakarta.validation.constraints.Size
-import javax.persistence.OneToMany
 import pt.up.fe.ni.website.backend.model.constants.ActivityConstants as Constants
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 abstract class Activity(
 
-        @JsonProperty(required = true)
+    @JsonProperty(required = true)
     @field:Size(min = Constants.Title.minSize, max = Constants.Title.maxSize)
     open val title: String,
 
-        @JsonProperty(required = true)
+    @JsonProperty(required = true)
     @field:Size(min = Constants.Description.minSize, max = Constants.Description.maxSize)
     open val description: String,
 
-        @OneToMany
+    @OneToMany
     val perRoles: List<PerActivityRole>,
 
-        @Id
+    @Id
     @GeneratedValue
     open val id: Long? = null
 )
