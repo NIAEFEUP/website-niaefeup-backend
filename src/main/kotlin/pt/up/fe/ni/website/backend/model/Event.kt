147ca6d57704c405b7c170f7f796b2294fb2d8ed
pedronunes19 <pedromiguelmnunes@gmail.com>
2023-01-09 02:47:51 +0000

    add url parameter to event

--- src/main/kotlin/pt/up/fe/ni/website/backend/model/Event.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/model/Event.kt
@@ -2,12 +2,17 @@ package pt.up.fe.ni.website.backend.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Entity
+import org.hibernate.validator.constraints.URL
 import java.util.Date
 
 @Entity
 class Event(
     title: String,
     description: String,
+
+    @JsonProperty(required = true)
+    @field:URL
+    var registerUrl: String,
 
     @JsonProperty(required = true)
     val date: Date,
