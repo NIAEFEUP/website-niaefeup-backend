933aa498df21b0e8386631fa9fe45b31f08d211f
André Lima <me@limwa.pt>
2023-01-09 02:18:05 +0000

    fix: make permissions converter a jpa converter

--- src/main/kotlin/pt/up/fe/ni/website/backend/permissions/PermissionsConverter.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/permissions/PermissionsConverter.kt
@@ -1,7 +1,9 @@
 package pt.up.fe.ni.website.backend.permissions
 
 import javax.persistence.AttributeConverter
+import javax.persistence.Converter
 
+@Converter
 class PermissionsConverter : AttributeConverter<Permissions, Long> {
     override fun convertToDatabaseColumn(attribute: Permissions) = attribute.toLong()
     override fun convertToEntityAttribute(dbData: Long) = Permissions.fromLong(dbData)
