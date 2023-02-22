54f09a7cdf1e33df86a6a90132b46af3f8113330
coutinho21 <guilhermecscoutinho@gmail.com>
2023-02-22 22:33:37 +0000

    Created methods and endpoint for password change

--- src/main/kotlin/pt/up/fe/ni/website/backend/controller/AccountController.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/controller/AccountController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pt.up.fe.ni.website.backend.dto.auth.ChangePassDto
 import pt.up.fe.ni.website.backend.dto.entity.AccountDto
 import pt.up.fe.ni.website.backend.service.AccountService
 
@@ -20,4 +21,10 @@ class AccountController(private val service: AccountService) {
 
     @PostMapping("/new")
     fun createAccount(@RequestBody dto: AccountDto) = service.createAccount(dto)
+
+    @PostMapping("/changePassword/{id}")
+    fun changePassword(@PathVariable id: Long, @RequestBody dto: ChangePassDto): Map<String, String> {
+        service.changePassword(id, dto)
+        return mapOf()
+    }
 }
