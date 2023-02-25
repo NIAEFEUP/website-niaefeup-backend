445a092790562f3d0e8f4bb2904945d58d3cd364
toino <joaoapereira21@hotmail.com>
2023-02-25 11:01:12 +0000

    Implement email attachments

--- src/main/kotlin/pt/up/fe/ni/website/backend/email/TemplateEmailBuilder.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/email/TemplateEmailBuilder.kt
@@ -1,6 +1,7 @@
 package pt.up.fe.ni.website.backend.email
 
 import com.samskivert.mustache.Mustache
+import java.io.ByteArrayInputStream
 import org.springframework.boot.autoconfigure.mustache.MustacheResourceTemplateLoader
 import org.springframework.mail.javamail.MimeMessageHelper
 import pt.up.fe.ni.website.backend.config.email.EmailConfigProperties
@@ -8,10 +9,10 @@ import pt.up.fe.ni.website.backend.config.email.EmailConfigProperties
 abstract class TemplateEmailBuilder<T> : BaseEmailBuilder() {
     private var data: T? = null
 
-    open val htmlTemplatePath: String? = null
-    open val textTemplatePath: String? = null
-
-    protected open fun subject(data: T?): String = ""
+    protected open fun subject(data: T?): String? = null
+    protected open fun htmlTemplatePath(data: T?): String? = null
+    protected open fun textTemplatePath(data: T?): String? = null
+    protected open fun attachments(data: T?): List<TemplateAttachment> = emptyList()
 
     fun data(data: T) = apply {
         this.data = data
@@ -20,19 +21,24 @@ abstract class TemplateEmailBuilder<T> : BaseEmailBuilder() {
     override fun build(helper: MimeMessageHelper, emailConfigProperties: EmailConfigProperties) {
         super.build(helper, emailConfigProperties)
 
-        helper.setSubject(subject(data))
+        val subject = subject(data)
+        if (subject != null) {
+            helper.setSubject(subject)
+        }
 
         val mustache = Mustache.compiler().withLoader(
             MustacheResourceTemplateLoader(emailConfigProperties.templatePrefix, emailConfigProperties.templateSuffix)
         )
-        var text: String? = null
-        var html: String? = null
 
+        var text: String? = null
+        val textTemplatePath = textTemplatePath(data)
         if (textTemplatePath != null) {
             val textTemplate = mustache.loadTemplate(textTemplatePath)
             text = textTemplate.execute(data)
         }
 
+        var html: String? = null
+        val htmlTemplatePath = htmlTemplatePath(data)
         if (htmlTemplatePath != null) {
             val htmlTemplate = mustache.loadTemplate(htmlTemplatePath)
             html = htmlTemplate.execute(data)
@@ -45,5 +51,16 @@ abstract class TemplateEmailBuilder<T> : BaseEmailBuilder() {
         } else if (html != null) {
             helper.setText(html, true)
         }
+
+        for (attachment in attachments(data)) {
+            val template = mustache.loadTemplate(attachment.path(data))
+            val content = template.execute(data)
+            helper.addAttachment(attachment.name(data)) { ByteArrayInputStream(content.encodeToByteArray()) }
+        }
+    }
+
+    protected abstract inner class TemplateAttachment {
+        abstract fun path(data: T?): String
+        abstract fun name(data: T?): String
     }
 }
