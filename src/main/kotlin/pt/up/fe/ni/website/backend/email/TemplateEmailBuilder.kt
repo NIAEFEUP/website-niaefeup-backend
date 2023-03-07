0604abc300fe82a04278504eeac48e4d366f2db5
toino <joaoapereira21@hotmail.com>
2023-03-07 18:54:30 +0000

    Use markdown for email templates and add support for inline files

--- src/main/kotlin/pt/up/fe/ni/website/backend/email/TemplateEmailBuilder.kt
+++ src/main/kotlin/pt/up/fe/ni/website/backend/email/TemplateEmailBuilder.kt
@@ -1,18 +1,21 @@
 package pt.up.fe.ni.website.backend.email
 
 import com.samskivert.mustache.Mustache
-import java.io.ByteArrayInputStream
+import org.commonmark.ext.front.matter.YamlFrontMatterExtension
+import org.commonmark.ext.front.matter.YamlFrontMatterVisitor
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
+import org.commonmark.renderer.text.TextContentRenderer
 import org.springframework.boot.autoconfigure.mustache.MustacheResourceTemplateLoader
+import org.springframework.core.io.Resource
+import org.springframework.core.io.UrlResource
 import org.springframework.mail.javamail.MimeMessageHelper
 import pt.up.fe.ni.website.backend.config.email.EmailConfigProperties
 
-abstract class TemplateEmailBuilder<T> : BaseEmailBuilder() {
+abstract class TemplateEmailBuilder<T>(
+    private val template: String
+) : BaseEmailBuilder() {
     private var data: T? = null
-
-    protected open fun subject(data: T?): String? = null
-    protected open fun htmlTemplatePath(data: T?): String? = null
-    protected open fun textTemplatePath(data: T?): String? = null
-    protected open fun attachments(data: T?): List<TemplateAttachment> = emptyList()
 
     fun data(data: T) = apply {
         this.data = data
@@ -21,46 +24,51 @@ abstract class TemplateEmailBuilder<T> : BaseEmailBuilder() {
     override fun build(helper: MimeMessageHelper, emailConfigProperties: EmailConfigProperties) {
         super.build(helper, emailConfigProperties)
 
-        val subject = subject(data)
-        if (subject != null) {
-            helper.setSubject(subject)
-        }
-
         val mustache = Mustache.compiler().withLoader(
             MustacheResourceTemplateLoader(emailConfigProperties.templatePrefix, emailConfigProperties.templateSuffix)
         )
 
-        var text: String? = null
-        val textTemplatePath = textTemplatePath(data)
-        if (textTemplatePath != null) {
-            val textTemplate = mustache.loadTemplate(textTemplatePath)
-            text = textTemplate.execute(data)
+        val markdown = mustache.loadTemplate(template).execute(data)
+
+        val commonmarkParser = Parser.builder().extensions(
+            listOf(
+                YamlFrontMatterExtension.create()
+            )
+        ).build()
+        val commonmarkHtmlRenderer = HtmlRenderer.builder().build()
+        val commonmarkTextRenderer = TextContentRenderer.builder().build()
+
+        val doc = commonmarkParser.parse(markdown)
+
+        val html = commonmarkHtmlRenderer.render(doc)
+        val text = commonmarkTextRenderer.render(doc)
+
+        helper.setText(text, html)
+
+        val yamlVisitor = YamlFrontMatterVisitor()
+        doc.accept(yamlVisitor)
+
+        val subject = yamlVisitor.data["subject"]?.getOrNull(0)
+        if (subject != null) {
+            helper.setSubject(subject)
         }
 
-        var html: String? = null
-        val htmlTemplatePath = htmlTemplatePath(data)
-        if (htmlTemplatePath != null) {
-            val htmlTemplate = mustache.loadTemplate(htmlTemplatePath)
-            html = htmlTemplate.execute(data)
-        }
+        print(yamlVisitor.data)
 
-        if (text != null && html != null) {
-            helper.setText(text, html)
-        } else if (text != null) {
-            helper.setText(text)
-        } else if (html != null) {
-            helper.setText(html, true)
-        }
-
-        for (attachment in attachments(data)) {
-            val template = mustache.loadTemplate(attachment.path(data))
-            val content = template.execute(data)
-            helper.addAttachment(attachment.name(data)) { ByteArrayInputStream(content.encodeToByteArray()) }
-        }
+        yamlVisitor.data.getOrDefault("attachments", emptyList()).forEach { addFile(helper::addAttachment, it) }
+        yamlVisitor.data.getOrDefault("inline", emptyList()).forEach { addFile(helper::addInline, it) }
     }
 
-    protected abstract inner class TemplateAttachment {
-        abstract fun path(data: T?): String
-        abstract fun name(data: T?): String
+    private fun addFile(fn: (String, Resource) -> Any, file: String) {
+        val split = file.split("\\s*::\\s*".toRegex())
+
+        if (split.size != 2) {
+            return
+        }
+
+        val name = split[0]
+        val path = split[1]
+
+        fn(name, UrlResource(path))
     }
 }
