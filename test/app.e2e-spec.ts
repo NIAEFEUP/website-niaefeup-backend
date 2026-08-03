288c52d592f9c338c206cc5b5a90d5c8d4bf3692
Pedro Castro <falecompedroac@gmail.com>
2026-08-03 11:59:17 +0100

    chore: updated biome config (noRestrictedImports)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '@/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
