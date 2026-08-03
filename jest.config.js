1a3e6f4df9b443544cbea33a2ba46b8daa79a079
Pedro Castro <falecompedroac@gmail.com>
2026-08-03 12:54:01 +0100

    chore: split jest config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('jest');
+
+module.exports = defineConfig({
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+});
