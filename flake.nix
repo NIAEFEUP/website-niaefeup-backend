73ba1fbd8e289338dcc51a3e980c43836755af30
Pedro Castro <falecompedroac@gmail.com>
2026-08-24 18:12:29 +0100

    chore(deps): bump nodejs from 22 to 24

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            nodejs_22
+            nodejs_24
             nest-cli
             postgresql_16
             docker-compose
